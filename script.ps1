b151d5e1392c650ac836fa9c7252783fc523bac9
konattsu <kokonattsu1112@gmail.com>
2025-01-24 00:29:14 +0900

    [fix] script

--- script.ps1
+++ script.ps1
@@ -1,6 +1,6 @@
 git clone --depth 1 "https://github.com/konattsu/my_rust_template"
 Remove-Item .\my_rust_template\.git -Force -Recurse
 Remove-Item .\my_rust_template\script.ps1 -Force
-Remove-Item .\my_rust_template\README.md -Force
-xcopy .\my_rust_template /s /e
+Remove-Item .\my_rust_template\README.* -Force
+xcopy .\my_rust_template .\ /s /e
 Remove-Item .\my_rust_template -Force -Recurse
