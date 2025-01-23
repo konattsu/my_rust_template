19d4231da0d1c51a99d8483c751321bc409b5e8d
konattsu <kokonattsu1112@gmail.com>
2025-01-24 00:15:06 +0900

    [update] 自動でフェッチしてくれるスクリプト追加

---
+++ script.ps1
@@ -0,0 +1,5 @@
+git clone --depth 1 "https://github.com/konattsu/my_rust_template"
+Remove-Item .\my_rust_template\.git -Force -Recurse
+Remove-Item .\my_rust_template\script.ps1 -Force
+xcopy .\my_rust_template /s /e
+Remove-Item .\my_rust_template -Force -Recurse
